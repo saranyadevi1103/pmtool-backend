a9b9709fad15be932425c29ec4d975b16939cafe
Saranya Devi Sankar <saranyadevi1103@gmail.com>
2025-07-25 23:33:16 +0530

    Fix merge conflict in user.js

--- models/user.js
+++ models/user.js
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
 // backend/models/User.js
-
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
@@ -10,16 +8,3 @@ const userSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model("User", userSchema);
-=======
-// backend/models/User.js
-
-const mongoose = require("mongoose");
-
-const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-});
-
-module.exports = mongoose.model("User", userSchema);
->>>>>>> 52dd01c52d5d779a24bf328da073b58c3b304f6f
